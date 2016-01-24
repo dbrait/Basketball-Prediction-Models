0160a05489c5514825fe8696f7d29b7180b67448
dbrait <david.brait@gmail.com>
2016-01-24 13:40:56 -0500

    mainly adding data

--- Nba package lod.R
+++ Nba package lod.R
@@ -24,86 +24,128 @@ get_nba_teams_seasons_roster(team = "Nets", include_coaches = F)
 
 get_team_season_shot_data(team = "Nets", year_data=2015)
 
-plot_nba_team_season_bokeh_shotchart(team = "Nets", year_roster = 2016,
-                                     year_data = 2015, plot_hex = F,
-                                     use_shot_zone_side = T,
-                                     season_type = c("Pre Season"))
-plot_nba_player_bokeh_shotchart(player = "Brook Lopez", plot_hex = F, vs_conference = "East",
-                                year_season_end = 2015)
-
-
-get_batch_player_gamelogs(year.season_start=2015, season_type="Regular")
-
-
-get_nba_synergy_stats (table_name = "Transition", include_defense = T, include_offense = T, type_table = "team", return_message = T)
-
-
-get_player_season_shot_data(player="Kyle Lowry")
-
-
-
-
-get_year_draft_combine(combine_year = 2009, return_message=T)
-
-get_all_draft_data(draft_years = 1960:2015)
-
-PlayerIso <- get_nba_synergy_stats(table_name="Isolation",
-                      include_defense=T,
-                      include_offense=T,
-                      type_table="player")
 
 #sort through player iso stats
 PlayerIso %>% filter(possesions > 100) %>% filter(pct.play_type > 15)
 
+#Get team defensive stats for each table type
+teamDefense <- get_nba_synergy_stats(include_offense=F, 
+                                     include_defense=T, 
+                                     type_table="team")
 
-
-TeamTransition <- get_nba_synergy_stats(table_name="Transition",
-                      include_defense=T,
-                      include_offense=T,
-                      type_table="team",
-                      return_message=T)
-
-SpotUp <- get_nba_synergy_stats(table_name="Spot-Up",
-                                include_defense=T,
-                                include_offense=T,
-                                type_table="player")
-
-PickandRoll <- get_nba_synergy_stats(table_name="Pick & Roll, Ball Handler",
-                                     include_defense=T,
-                                     include_offense=T,
-                                     type_table="player")
-
-OffScreen <- get_nba_synergy_stats(table_name="Pick & Roll, Roll Man",
-                                   include_defense=T,
-                                   include_offense=T,
-                                   type_table="player")
-
-OffensiveRebounds <- get_nba_synergy_stats(table_name="Offensive Rebounds",
-                                           include_defense=T,
-                                           include_offense=T,
-                                           type_table="player")
-
-HandOff <- get_nba_synergy_stats(table_name="Handoff",
-                                 include_defense=T,
-                                 include_offense=T,
-                                 type_table="player")
-
-OffensiveRebounds %>% filter(pct.play_type > 10)
-
-#Breaking down team defense, create clustering and visualizations for each
-teamDefense <- get_all_team_synergy_stats(include_defense=T,
-                                   include_offense=F)
 
 write.csv(teamDefense, "Team Defense.csv")
 
-#PLay tendencies
-percRollman <- teamDefense %>% select(team, pct.play_type.prrollman)
-ptsRollman <- teamDefense %>% select(team, pts.per_game.prrollman)
+defCut <- get_nba_synergy_stats(table_name="Cut",
+                                include_defense=T,
+                                include_offense=F,
+                                type_table="team")
 
-ptsBallhandler <- teamDefense %>% select(team, pts.per_game.prballhandler)
-percBallhandler <- teamDefense %>% select(team, pct.play_type.prballhandler)
+defteamIsolation <- get_nba_synergy_stats(table_name="Isolation",
+                                          include_offense=F,
+                                          include_defense=T,
+                                          type_table="team")
 
-percIso <- teamDefense %>% select(team, pct.play_type.isolation)
+defteamOffScreen <- get_nba_synergy_stats(table_name="Off Screen",
+                                          include_offense=F,
+                                          include_defense=T,
+                                          type_table="team")
+
+defteamOReb <- get_nba_synergy_stats(table_name="Offensive Rebounds",
+                                     include_offense=F,
+                                     include_defense=T,
+                                     type_table="team")
+
+defTeamPostUp <- get_nba_synergy_stats(table_name="Post-Up",
+                                       include_offense=F,
+                                       include_defense=T,
+                                       type_table="team")
+
+defBallHandler <- get_nba_synergy_stats(table_name="Pick & Roll, Ball Handler",
+                                        include_offense=F,
+                                        include_defense=T,
+                                        type_table="team")
+
+defTeamHandoff <- get_nba_synergy_stats(table_name="Handoff",
+                                        include_offense=F,
+                                        include_defense=T,
+                                        type_table="team")
+
+defTeamMisc <- get_nba_synergy_stats(table_name="Miscellanous",
+                                     include_offense=F,
+                                     include_defense=T,
+                                     type_table="team")
+
+defTeamPickAndRollRollMan <- get_nba_synergy_stats(table_name="Pick & Roll, Roll Man",
+                                                   include_offense=F,
+                                                   include_defense=T,
+                                                   type_table="team")
+
+defTeamSpotUp <- get_nba_synergy_stats(table_name="Spot-Up",
+                                       include_offense=F,
+                                       include_defense=T,
+                                       type_table="team")
+
+defTeamTransition <- get_nba_synergy_stats(table_name="Transition",
+                                           include_offense=F,
+                                           include_defense=T,
+                                           type_table="team")
+
+#Individual Players Offensive Tendencies
+playerIso <- get_nba_synergy_stats(table="Isolation",
+                                   include_offense=T,
+                                   include_defense=F,
+                                   type_table="player")
+
+playerPostUp <- get_nba_synergy_stats(table="Post-Up",
+                                      include_offense=T,
+                                      include_defense=F,
+                                      type_table="player")
+
+playerPickAndRollBallHandler <- get_nba_synergy_stats(table="Pick & Roll, Ball Handler",
+                                                      include_offense=T,
+                                                      include_defense=F,
+                                                      type_table="player")
+
+playerCut <- get_nba_synergy_stats(table="Cut",
+                                   include_offense=T,
+                                   include_defense=F,
+                                   type_table="player")
+
+playerHandoff <- get_nba_synergy_stats(table="Handoff",
+                                       include_offense=T,
+                                       include_defense=F,
+                                       type_table="player")
+
+playerMisc <- get_nba_synergy_stats(table="Miscellanous",
+                                    include_offense=T,
+                                    include_defense=F,
+                                    type_table="player")
+
+playerOffScreen <- get_nba_synergy_stats(table="Off Screen",
+                                         include_offense=T,
+                                         include_defense=F,
+                                         type_table="player")
+
+playerOReb <- get_nba_synergy_stats(table="Offensive Rebounds",
+                                    include_offense=T,
+                                    include_defense=F,
+                                    type_table="player")
+
+playerRollMan <- get_nba_synergy_stats(table="Pick & Roll, Roll Man",
+                                                  include_offense=T,
+                                                  include_defense=F,
+                                                  type_table="player")
+
+playerSpotUp <- get_nba_synergy_stats(table_name="Spot-Up",
+                                      include_offense=T,
+                                      include_defense=F,
+                                      type_table="player")
+
+playerTransition <- get_nba_synergy_stats(table="Transition",
+                                          include_offense=T,
+                                          include_defense=F,
+                                          type_table="player")
 
 
 #Tendencies
@@ -112,9 +154,6 @@ percPlays <- teamDefense %>% select(team, pct.play_type.cut, pct.play_type.hando
                                     pct.play_type.transition)
 
 write.csv(percPlays, "Percentage of Plays.csv")
-
-percPlays %>% filter(pct.play_type.prballhandler > 18.5)
-percPlays %>% filter(pct.play_type.transition > 14)
 
 #giving up ORebs
 OffReb <- teamDefense %>% select(team, pct.play_type.offrebound, pts.offrebound, ppp.offrebound, possesions.per_game.offrebound, possesions.offrebound,
@@ -131,174 +170,41 @@ offTransition <- get_nba_synergy_stats(table_name="Transition",
                                        include_offense=T,
                                        type_table="team")
 
-playerTransition <- get_nba_synergy_stats(table_name="Transition",
-                                          include_defense=F,
-                                          include_offense=T,
-                                          type_table="player")
+#teams in the pick and roll offense
+teamRollMan <- get_nba_synergy_stats(table_name="Pick & Roll, Ball Handler",
+                                     include_offense=T,
+                                     include_defense=F,
+                                     type_table="team")
 
+teamBallHandler <- get_nba_synergy_stats(table_name="Pick & Roll, Roll Man",
+                                         include_offense=T,
+                                         include_defense=F,
+                                         type_table="team")
 
-#team offense spot up
-offSpotup <- get_nba_synergy_stats(table_name="Spot-Up",
-                                   include_defense=F,
-                                   include_offense=T,
-                                   type_table="team")
-
-playerSpotup <- get_nba_synergy_stats(table_name="Spot-Up",
-                                      include_defense=F,
-                                      include_offense=T,
-                                      type_table="player")
-
-#team offense post up
-offPostUp <- get_nba_synergy_stats(table="Post-Up",
-                                   include_defense=F,
-                                   include_offense=T,
-                                   type_table="team")
-
-playerPostUp <- get_nba_synergy_stats(table="Post-Up",
-                                      include_defense=F,
-                                      include_offense=T,
-                                      type_table="player")
-
-#team offense pick and rolls
-offBallHandler <- get_nba_synergy_stats(table="Pick & Roll, Ball Handler",
-                                        include_defense=F,
-                                        include_offense=T,
-                                        type_table="team")
-
-playerBallHandler <- get_nba_synergy_stats(table="Pick & Roll, Ball Handler",
-                                           include_defense=F,
-                                           include_offense=T,
-                                           type_table="player")
-
-offRollMan <- get_nba_synergy_stats(table="Pick & Roll, Roll Man",
-                                    include_defense=F,
-                                    include_offense=T,
-                                    type_table="team")
-
-playerRollMan <- get_nba_synergy_stats(table="Pick & Roll, Roll Man",
-                                       include_defense=F,
-                                       include_offense=T,
-                                       type_table="player")
-
-#isolation
+#teams isolation use
 offIsolation <- get_nba_synergy_stats(table="Isolation",
                                       include_defense=F,
                                       include_offense=T,
                                       type_table="team")
 
-playerIsolation <- get_nba_synergy_stats(table="Isolation",
-                                         include_defense=F,
-                                         include_offense=T,
-                                         type_table="player")
-
-defIsolation <- get_nba_synergy_stats(table="Isolation",
-                                      include_defense=T,
-                                      include_offense=F,
-                                      type_table="player")
-
-defteamIsolation <- get_nba_synergy_stats(table="Isolation",
-                                          include_offense=F,
-                                          include_defense=T,
-                                          type_table="team")
-
-#cuts
+#teams use of cuts
 offCut <- get_nba_synergy_stats(table="Cut",
                                 include_offense=T,
                                 include_defense=F,
                                 type_table="team")
 
-summary(offCut)
-heavyTeamCut <- offCut %>% arrange(fga.per_game)
-
-playerCut <- get_nba_synergy_stats(table="Cut",
-                                   include_offense=T,
-                                   include_defense=F,
-                                   type_table="player")
-summary(playerCut)
-heavyPlayerCut <- playerCut %>% arrange(desc(fga.per_game))
-
-
-defCut <- get_nba_synergy_stats(table="Cut",
-                                include_offense=F,
-                                include_defense=T,
-                                type_table="team")
-
-summary(defCut)
-heavyDefCut <- defCut %>% arrange(desc(fga.per_game))
-
-#off screen
+#team off screen
 offScreen <- get_nba_synergy_stats(table="Off Screen",
                                    include_offense=T,
                                    include_defense=F,
                                    type_table="team")
 
-summary(offScreen)
-
-playerOffScreen <- get_nba_synergy_stats(table="Off Screen",
-                                         include_offense=T,
-                                         include_defense=F,
-                                         type_table="player")
-
-summary(playerOffScreen)
-heavyOffScreen <- playerOffScreen %>% filter(fga > 50) %>% arrange(pct.efg)
-
-defteamOffScreen <- get_nba_synergy_stats(table="Off Screen",
-                                          include_offense=F,
-                                          include_defense=T,
-                                          type_table="team")
-
-summary(defteamOffScreen)
-defHeavyOffScreen <- defteamOffScreen %>% arrange(desc(fga))
-defPercOffScreen <- defteamOffScreen %>% arrange(desc(pct.efg))
-
-#OReb
+#teams OReb
 
 offReb <- get_nba_synergy_stats(table="Offensive Rebounds",
                                 include_offense=T,
                                 include_defense=F,
                                 type_table="team")
-
-summary(offReb)
-heavyTeamOffReb <- offReb %>% arrange(desc(fga.per_game))
-
-playerOffReb <- get_nba_synergy_stats(table="Offensive Rebounds",
-                                      include_offense=T,
-                                      include_defense=F,
-                                      type_table="player")
-
-summary(playerOffReb)
-heavyPlayerOffReb <- playerOffReb %>% arrange(desc(fga.per_game))
-
-teamdefOReb <- get_nba_synergy_stats(table="Offensive Rebounds",
-                                     include_offense=F,
-                                     include_defense=T,
-                                     type_table="team")
-summary(teamdefOReb)
-heavyDefOffReb <- teamdefOReb %>% arrange(desc(fga.per_game))
-
-#work through team defense
-
-
-#Individual Player breakdowns
-
-TeamOffense <- get_nba_synergy_stats(table_name="Transition", "Offensive Rebounds", "Off Screen",
-                                     include_defense=F,
-                                     include_offense=T,
-                                     type_table="team")
-                                     
-
-
-
-indTransitionOff <- get_nba_synergy_stats(table_name="Transition",
-                                          include_offense=T,
-                                          include_defense=F)
-
-
-get_fanduel_bref_players()
-get_headers_css_data()
-get_bref_player_season_stat_table()
-  
-
 
 #SOMs
 library(dplyr)
