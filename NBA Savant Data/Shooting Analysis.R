c76c782ede3de134aea317e991528e1855e969e2
dbrait <david.brait@gmail.com>
2016-04-05 17:12:40 -0400

    updated shooting analysis

--- NBA Savant Data/Shooting Analysis.R
+++ NBA Savant Data/Shooting Analysis.R
@@ -1,4 +1,8 @@
+library(dplyr)
+library(ggplot2)
 #Nba shooting analysis with nba savant data
+
+setwd("C:/Users/My Files/Desktop/Data Science Resources/Basketball Prediction Models/NBA Savant Data")
 Shooting_2010 <- read.csv("2010 Shooting.csv")
 summary(Shooting_2010)
 
@@ -17,3 +21,30 @@ summary(Shooting_2014)
 Shooting_2015 <- read.csv("2015 Shooting.csv")
 summary(Shooting_2015)
 
+tally(shot_made_flag ~ shot_type, data= Shooting_2015, format="proportion")
+tally(shot_made_flag ~ shot_type, data= Shooting_2014, format="proportion")
+tally(shot_made_flag ~ shot_type, data = Shooting_2013, format="proportion")
+tally(shot_made_flag ~ shot_type, data = Shooting_2012, format="proportion")
+tally(shot_made_flag ~ shot_type, data = Shooting_2011, format="proportion")
+
+tally(shot_made_flag ~ action_type, data = Shooting_2015, format="proportion")
+tally(shot_made_flag ~ action_type, data = Shooting_2014, format="proportion")
+
+
+
+#investigate team defense specific trends
+Opp <- Shooting_2015 %>% group_by(opponent) %>% filter(shot_type == "2PT Field Goal") %>% summarise(count = n(),
+                                                                                                    shot_dist = mean(shot_distance),
+                                                                                                    def_dist = mean(defender_distance))
+
+#3pt defense
+Opp_Three <- Shooting_2015 %>% group_by(opponent) %>% filter(shot_type == "3PT Field Goal") %>% summarise(count = n(),
+                                                                                                          shot_dist = mean(shot_distance),
+                                                                                                          def_dist = mean(defender_distance))
+
+
+#Catch and shoot
+Catch_And_Shoot <- Shooting_2015 %>% group_by(opponent) %>% filter(touch_time < 1.1) %>% summarise(count = n())
+
+#early shot clock
+Early_Shot <- Shooting_2015 %>% group_by(opponent) %>% filter(shot_clock < 10) %>% summarise(count = n())
